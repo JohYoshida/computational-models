3980748de008df1a50ad6a73b028a70b95f8f60f
Joh Yoshida <johyoshida@gmail.com>
2019-08-20 18:21:46 -0700

    Displays range and number by default, pass false by props to hide

--- components/InputNumber.jsx
+++ components/InputNumber.jsx
@@ -1,25 +1,34 @@
 export default function InputNumber(props) {
+
+  const range = (props.range === false) ? null : (
+    <input
+      type="range"
+      name="X"
+      min={typeof props.min !== "undefined" ? props.min : "1"}
+      max={typeof props.max !== "undefined" ? props.max : "256"}
+      step="1"
+      value={props.val}
+      onChange={evt => props.updateVal(evt)}
+    />
+  );
+
+  const number = (props.number === false) ? null : (
+    <input
+      style={inputStyle}
+      type="number"
+      min={typeof props.min !== "undefined" ? props.min : "1"}
+      max={typeof props.max !== "undefined" ? props.max : "256"}
+      step="1"
+      value={props.val}
+      onChange={evt => props.updateVal(evt)}
+    />
+  );
+
   return (
     <div>
       <label>{props.name}</label>
-      <input
-        type="range"
-        name="X"
-        min={typeof props.min !== "undefined" ? props.min : "1"}
-        max={typeof props.max !== "undefined" ? props.max : "256"}
-        step="1"
-        value={props.val}
-        onChange={evt => props.updateVal(evt)}
-      />
-      <input
-        style={inputStyle}
-        type="number"
-        min={typeof props.min !== "undefined" ? props.min : "1"}
-        max={typeof props.max !== "undefined" ? props.max : "256"}
-        step="1"
-        value={props.val}
-        onChange={evt => props.updateVal(evt)}
-      />
+      { range }
+      { number }
     </div>
   );
 }
