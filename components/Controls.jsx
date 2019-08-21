b2375fc540d5eb3591f7b5fd03ef4ac9ae397017
Joh Yoshida <johyoshida@gmail.com>
2019-08-20 22:41:43 -0700

    Added box shadow

--- components/Controls.jsx
+++ components/Controls.jsx
@@ -1,12 +1,18 @@
 export default function Controls(props) {
-  return <div style={controlsStyle}>{props.children}</div>;
+  return (
+    <div style={controlsStyle}>
+      {props.children}
+    </div>
+  );
 }
 
 // Styles
 
 const controlsStyle = {
   display: "flex",
+  justifyContent: "center",
   alignItems: "center",
-  flexDirection: "column",
-  marginBottom: 20
+  flexDirection: "row",
+  boxShadow: "0px 6px 20px rgba(0, 0, 0, 0.06)",
+  margin: 20
 };
