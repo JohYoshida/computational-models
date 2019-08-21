a73eb569e60521d13e0fc09b1db225902e9d1eee
Joh Yoshida <johyoshida@gmail.com>
2019-08-20 18:50:20 -0700

    Added Button compoennt that accepts a name and onClick function

---
+++ components/Button.jsx
@@ -0,0 +1,11 @@
+export default function Button(props) {
+  return (
+    <button type="button" onClick={props.onPress}>
+      { props.name ? props.name : "Button" }
+    </button>
+  );
+}
+
+const buttonStyle = {
+
+}
