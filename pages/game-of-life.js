409ebe4be66412a709bd7e876c86af27ba5a9933
Joh Yoshida <johyoshida@gmail.com>
2019-08-26 23:22:03 -0700

    Laid out input controls and buttons

--- pages/game-of-life.js
+++ pages/game-of-life.js
@@ -8,15 +8,80 @@ export default class GameOfLIfe extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      x: 25,
+      y: 25,
+      stage: 0,
+      data: [{}]
     };
   }
 
   render() {
+    const { x, y, stage, data } = this.state;
     return (
       <Layout>
         <Controls>
+          <div style={columnStyle}>
+            <InputNumber
+              name={"X"}
+              val={x}
+              updateVal={this.updateInputX}
+              displayValue={false}
+            />
+            <InputNumber
+              name={"Y"}
+              val={y}
+              updateVal={this.updateInputY}
+              displayValue={false}
+            />
+          // TODO: add onPress functions
+            <div style={rowStyle}>
+              <Button name="Randomize" onPress={""} />
+              <Button name="Reset" onPress={""} />
+              <Button name="Step Forward One Stage" onPress={""} />
+              <Button name="Evolve!" onPress={""} />
+            </div>
+            // TODO: add max value
+            <InputNumber
+              name={"Stage"}
+              val={stage}
+              updateVal={this.updateInputStage}
+              displayNumber={false}
+            />
+            <div style={rowStyle}>
+              <Button name="Cell" onPress={""} />
+              <Button name="Diamond" onPress={""} />
+              <Button name="Pulsar" onPress={""} />
+            </div>
+          </div>
         </Controls>
       </Layout>
     );
   }
+
+  updateInputX = evt => {
+    const x = Number(evt.target.value);
+    this.setState({ x });
+  }
+
+  updateInputY = evt => {
+    const y = Number(evt.target.value);
+    this.setState({ y });
+  }
+
+  updateInputStage = evt => {
+    const stage = Number(evt.target.value);
+    this.setState({ stage });
+  }
 }
+
+const rowStyle = {
+  display: "flex",
+  flexDirection: "row"
+};
+
+const columnStyle = {
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "center",
+  margin: 10
+};
