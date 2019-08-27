51386cb2cdfcfd807b01e021431ec8c2ff63567c
Joh Yoshida <johyoshida@gmail.com>
2019-08-26 23:17:49 -0700

    Skeleton

---
+++ pages/game-of-life.js
@@ -0,0 +1,14 @@
+import { Component } from "react";
+
+export default class GameOfLIfe extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+    };
+  }
+
+  render() {
+    return (
+    );
+  }
+}
