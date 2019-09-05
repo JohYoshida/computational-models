227296a9086c7798f6a136be8bd3f36e7c8176f8
Joh Yoshida <johyoshida@gmail.com>
2019-09-04 22:25:42 -0700

    Skeleton

---
+++ pages/geneticgame.js
@@ -0,0 +1,18 @@
+import { Component } from "react";
+import Layout from "../components/Layout";
+
+export default class GeneticGame extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+    };
+  }
+
+  render() {
+    return (
+      <Layout>
+        
+      </Layout>
+    );
+  }
+}
