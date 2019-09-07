e5689830ccc2206ab1eba45497bc94d8abd22dcb
Joh Yoshida <johyoshida@gmail.com>
2019-09-06 21:12:27 -0700

    Added function to translate gene sequence into a finite state machine agent

--- pages/geneticgame.js
+++ pages/geneticgame.js
@@ -51,4 +51,43 @@ export default class GeneticGame extends Component {
     }
     this.setState({ genePool });
   }
+
+  /**
+   * Translates a valid gene sequence into an Agent.
+   * @param  {String} sequence Binary gene sequence.
+   * @param  {Number} id       Sequence's index in gene pool.
+   * @return {Object}          Finite state machine Agent.
+   */
+  makeAgent = (sequence, id) => {
+    // Measure the binary digits required to designate each state transition;
+    const digits = (this.state.maxStates - 1).toString(2).length;
+    // Transliate initial state from sequence
+    const initial = parseInt(sequence.slice(0, digits), 2);
+    const states = [];
+    // Fill states
+    for (var i = 0; i < this.state.maxStates; i++) {
+      // Calculate starting index of state
+      let index = digits + i * (1 + (2 * digits));
+      // Translate state data from sequence
+      let state = {
+        move: parseInt(sequence.slice(index, index + 1)),
+        cooperative: parseInt(sequence.slice(index + 1, index + 5), 2),
+        cooperative: parseInt(sequence.slice(index + 1, index + 1 + digits), 2),
+        defective: parseInt(sequence.slice(index + 5, index + 1 + (2 * digits)), 2)
+      };
+      states.push(state);
+    }
+    // Return agent in form of finite state machine.
+    return {
+      id,
+      context: {
+        elapsed: 0,
+        performanceScore: 0,
+        current: initial
+      },
+      initial,
+      states
+    };
+  };
+
 }
