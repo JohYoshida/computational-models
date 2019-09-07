2df8a1f03f863586a5e50910aeedd2b1acffdf0c
Joh Yoshida <johyoshida@gmail.com>
2019-09-06 20:50:40 -0700

    Added function to generate gene sequence

--- pages/geneticgame.js
+++ pages/geneticgame.js
@@ -5,6 +5,7 @@ export default class GeneticGame extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      maxStates: 16,
     };
   }
 
@@ -14,5 +15,22 @@ export default class GeneticGame extends Component {
         
       </Layout>
     );
+  }
+
+  /**
+   * Generates a gene sequence of zeroes and ones. Length depends on maxStates
+   * @return { String } Gene sequence
+   */
+  generateGeneSequence() {
+    let sequence = "";
+    // Measure the binary digits required to designate each state transition;
+    const digits = (this.state.maxStates - 1).toString(2).length;
+    // Measure the length of the gene sequence
+    const length = digits + this.state.maxStates * (1 + (2 * digits));
+    for (var i = 0; i < length; i++) {
+      let gene = Math.floor(Math.random() * 2);
+      sequence += gene;
+    }
+    return sequence;
   }
 }
